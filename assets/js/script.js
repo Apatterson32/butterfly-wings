38ee38d2f2e1d2e0d5d6ddf8d92715efe671204d
Alissa <pattersonal10@gmail.com>
2023-10-09 20:40:36 -0500

    added js but fetch not working

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+const app = {
+    init: () => {
+        document.addEventListener('DOMContentLoaded', () => {
+            // Event listener to the "fetch-button"
+            const fetchButton = document.getElementById('fetch-button');
+            if (fetchButton) {
+                fetchButton.addEventListener('click', app.fetchWeather);
+            }
+        });
+    },
+
+    fetchWeather: () => {
+        const apiKey = '386b6c4c63e589662c2c';
+        const units = 'imperial';
+
+        // Get the city name from the input field
+        const cityName = document.getElementById('search').value;
+
+        const currentWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+
+        // Fetch current weather data
+        fetch(currentWeatherApiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Process and display the current weather data here
+                console.log('Current Weather Data:', data);
+
+                const cityNameElement = document.querySelector('.card-title');
+                const temperatureElement = document.querySelector('.card-text.temperature');
+                const windElement = document.querySelector('.card-text.wind');
+                const humidityElement = document.querySelector('.card-text.humidity');
+
+                // Update the elements with the weather data
+                cityNameElement.textContent = data.name;
+                temperatureElement.textContent = `Temperature: ${data.main.temp} °F`;
+                windElement.textContent = `Wind: ${data.wind.speed} mph`;
+                humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+            })
+            .catch(error => {
+                console.error('Error fetching current weather data:', error);
+            });
+    }
+};
+
+// Initialize the app
+app.init();
+
+
+
+
+
+
